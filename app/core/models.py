e25804d4c56dce8d72649d0602f786a614b678b5
martins <martin.santamaria@rga.com>
2022-07-31 18:52:40 -0300

    Solve auth classes asignment

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,6 @@
-from django.db import models
-
 """
 Database models.
 """
-
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
--- app/user/views.py
+++ app/user/views.py
@@ -24,7 +24,7 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authetication_classes = [authentication.TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
