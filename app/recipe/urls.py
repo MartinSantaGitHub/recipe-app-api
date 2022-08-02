da253492335da12ffad565a6daae8a9521a29fba
martins <martin.santamaria@rga.com>
2022-08-02 18:30:00 -0300

    Recipe

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -9,10 +9,10 @@ from rest_framework.routers import DefaultRouter
 from recipe import views
 
 router = DefaultRouter()
-router.register('recipe', views.RecipeViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('/', include(router.urls))
 ]
